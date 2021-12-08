81cd2fc8f2f8995d890b54556a6d120a17e9024f
elzodp125 <elzodp125@gmail.com>
2021-12-08 22:54:13 +0100

    modiff problem.cpp

--- src/probleme.cpp
+++ src/probleme.cpp
@@ -14,7 +14,7 @@ void Problem::solve()
 {
   double tmp_i=m_discretiz->get_initial_time() ;
   size_t N_i=iteration(double t) const;
-  Variable *y_init=new Variable[N_i];
+  Variable y_init[N_i];
   double pas_temp;
 
   m_eq.compute_initial_condition(tmp_i, y_init);
@@ -22,7 +22,7 @@ void Problem::solve()
   for (size_t step = 0; step < m_discretiz->get_nb_points(); step++)
   {
     double tmp = m_discretiz->get_pas() * step;
-    Variable *y=new Variable[N_i];
+    Variable y[N_i];
      
      m_eq.compute(tmp,pas_temp,y);
   }
